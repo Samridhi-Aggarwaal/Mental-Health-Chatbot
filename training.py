037f671ac432154f8c722196282cd18e7e9b423f
Samridhi <samridhi.agarwal001122@gmail.com>
2025-04-15 12:34:17 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,20 +1,34 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
+#Training chatbot model using NLP and nueral network built with Keras
+    #Takes pre-processed data (tokenization, lemmatization, BoW representation) from intents.json
+    #Trains neural network classifier
+    #Saves trained model in model.h5, vocabulary in texts.pkl, intent labels in label.pkl
+
+
 import json
+import nltk
 import pickle
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
 import random
+import numpy as np
+
+from keras.optimizers import SGD
+from keras.models import Sequential
+from nltk.stem import WordNetLemmatizer
+from keras.layers import Dense, Dropout
+
+# Initialize lemmatizer
+lemmatizer = WordNetLemmatizer()
+
+#Data containers
 words=[]
 classes = []
 documents = []
 ignore_words = ['?', '!']
-data_file = open('intents.json').read()
-intents = json.loads(data_file)
 
+# Load intents file
+with open('intents.json') as file:
+    intents = json.load(file)
+
+# Preprocessing: tokenize patterns, collect vocabulary and classes
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         #tokenize each word
@@ -25,24 +39,26 @@ for intent in intents['intents']:
         # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-# lemmatize and lower each word and remove duplicates
+
+# Lemmatize and lower each word and remove duplicates
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-# sort classes
+
+# sort intent classes
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
+
+# Data summary
 print (len(documents), "documents")
-# classes = intents
 print (len(classes), "classes", classes)
-# words = all words, vocabulary
 print (len(words), "unique lemmatized words", words)
+
+# Save processed words and classes
 pickle.dump(words,open('texts.pkl','wb'))
 pickle.dump(classes,open('labels.pkl','wb'))
-# create our training data
+
+# create training data
 training = []
-# create an empty array for our output
 output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
 for doc in documents:
     # initialize our bag of words
     bag = []
@@ -53,19 +69,22 @@ for doc in documents:
     # create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    
     # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-    
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
+
+# shuffle features and convert into np.array
 random.shuffle(training)
 training = np.array(training)
+
 # create train and test lists. X - patterns, Y - intents
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
+
+
+# Build the Neural Network
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
@@ -74,10 +93,13 @@ model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+
+# Compile model
+# Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-#fitting and saving the model 
+# Model training
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+# Model saved
 model.save('model.h5', hist)
 print("model created")
